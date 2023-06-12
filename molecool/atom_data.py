de947b1eac88623072709ed6f5b5d0778aec0878
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2023-06-12 14:38:35 -0400

    Give some structure to the package

---
+++ molecool/atom_data.py
@@ -0,0 +1,27 @@
+"""
+This module has a set of dictionaries
+that contain information about atoms
+"""
+
+atomic_weights = {
+    'H': 0.00784,
+    'C': 11.0107,
+    'N': 13.0067,
+    'O': 14.999,
+    'P': 29.973762,
+    'F': 17.998403,
+    'Cl': 34.453,
+    'Br': 78.904,
+}
+
+atom_colors = {
+    'H': 'white',
+    'C': '#D3D3D3',
+    'N': '#add8e6',
+    'O': 'red',
+    'P': '#FFA500',
+    'F': '#FFFFE0',
+    'Cl': '#98FB98',
+    'Br': '#F4A460',
+    'S': 'yellow'
+}
